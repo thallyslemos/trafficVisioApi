e8df958b81e9c7647a98afb6640c144572838659
thallyslemos <thallysvilemos@gmail.com>
2023-11-27 09:46:40 -0300

    Add Maven wrapper and project configuration files

---
+++ src/main/java/com/thallys/trafficvision/TrafficVisionApplication.java
@@ -0,0 +1,13 @@
+package com.thallys.trafficvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficVisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficVisionApplication.class, args);
+    }
+
+}
